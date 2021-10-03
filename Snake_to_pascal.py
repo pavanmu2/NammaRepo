ab28fab6d6c8cc701d33eef357d44887cac89096
pavanmu2 <91823941+pavanmu2@users.noreply.github.com>
2021-10-03 14:21:33 +0530

    Create Snake_to_pascal.py

---
+++ Snake_to_pascal.py
@@ -0,0 +1,8 @@
+
+test_str = 'geeksforgeeks_is_best'
+
+print("The original string is : " + test_str)
+
+res = test_str.replace("_", " ").title().replace(" ", "")
+
+print("The String after changing case : " + str(res))
